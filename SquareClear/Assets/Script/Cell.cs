f77645185b4b766da98036a71b020a8546195413
murang <ddrddr3@126.com>
2016-04-21 16:51:05 +0800

    try to make a method to move the cube

--- SquareClear/Assets/Script/Cell.cs
+++ SquareClear/Assets/Script/Cell.cs
@@ -2,14 +2,56 @@
 using System.Collections;
 
 public class Cell : MonoBehaviour {
+	
+
+	private ArrayList m_colorList = new ArrayList(){
+		Color.black,
+		Color.blue,
+		Color.cyan,
+		Color.gray,
+		Color.green,
+		Color.magenta,
+		Color.red,
+		Color.white,
+		Color.yellow
+	};
+	public enum CELL_COLOR_TYPE{
+		BLACK = 0,
+		BLUE,
+		CYAN,
+		GRAY,
+		GREEN,
+		MAGENTA,
+		RED,
+		WHITE,
+		YELLOW
+	};
+	private CELL_COLOR_TYPE m_CellType;
+
+	enum CELL_MOVE_STATE{
+		STOP,
+		UP,
+		DOWN,
+		LEFT,
+		RIGHT
+	}
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void changeColor(CELL_COLOR_TYPE type){
+		m_CellType = type;
+		this.GetComponent<Renderer>().material.SetColor("_Color", (Color)m_colorList[(int)m_CellType]);
+	}
+
+	public void moveUp(){
+		transform.position=Vector3.MoveTowards(transform.position,transform.position+Vector3.one,2);
 	}
 }
--- SquareClear/Assets/Script/GameControl.cs
+++ SquareClear/Assets/Script/GameControl.cs
@@ -9,28 +9,7 @@ public class GameControl : MonoBehaviour {
 
 	private GameObject[,] cell_matrix = new GameObject[STAGE_RANGE*2+1,STAGE_RANGE*2+1];
 
-	enum CELL_TYPE{
-		BLACK = 0,
-		BLUE,
-		CYAN,
-		GRAY,
-		GREEN,
-		MAGENTA,
-		RED,
-		WHITE,
-		YELLOW
-	}
-	private ArrayList m_colorList = new ArrayList(){
-		Color.black,
-		Color.blue,
-		Color.cyan,
-		Color.gray,
-		Color.green,
-		Color.magenta,
-		Color.red,
-		Color.white,
-		Color.yellow
-	};
+
 //	private List<Color> m_colorList = new List<Color>();
 	// Use this for initialization
 
@@ -40,7 +19,9 @@ public class GameControl : MonoBehaviour {
 
 	void Start () {
 		cell_matrix[STAGE_RANGE, STAGE_RANGE] = (GameObject)Instantiate(pre_cell, new Vector3(STAGE_RANGE,STAGE_RANGE,0), Quaternion.identity);
-		cell_matrix[STAGE_RANGE, STAGE_RANGE].GetComponent<Renderer>().material.SetColor("_Color", (Color)m_colorList[Random.Range(0, m_colorList.Count)]);
+//		cell_matrix[STAGE_RANGE, STAGE_RANGE].SendMessage("changeColor",Cell.CELL_TYPE.RED);
+		cell_matrix[STAGE_RANGE, STAGE_RANGE].GetComponent<Cell>().changeColor(Cell.CELL_COLOR_TYPE.RED);
+//		cell_matrix[STAGE_RANGE, STAGE_RANGE].GetComponent<Renderer>().material.SetColor("_Color", (Color)m_colorList[Random.Range(0, m_colorList.Count)]);
 //		for(int x=0; x<STAGE_RANGE; x++){
 //			for(int y=0; y<STAGE_RANGE; y++){
 //				cell_matrix[x, y] = (GameObject)Instantiate(pre_cell, new Vector3((float)x,(float)y,0), Quaternion.identity);
@@ -48,6 +29,11 @@ public class GameControl : MonoBehaviour {
 //		}
 	}
 
+	void OnGUI(){
+		if(GUI.Button(new Rect(0,0,100,30),"click")){
+			cell_matrix[STAGE_RANGE, STAGE_RANGE].GetComponent<Cell>().moveUp();
+		}
+	}
 	
 	// Update is called once per frame
 	void Update () {
